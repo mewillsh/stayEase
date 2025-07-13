5143873c84715195c0f2fd0fcc057e09177f9b5b
mewillsh <harshsingh22082003@gmail.com>
2025-07-13 17:57:48 +0530

    Added Image Preview Functionality for our Edit page which renders lower quality Image from Cloudinary

--- controllers/listings.js
+++ controllers/listings.js
@@ -41,7 +41,9 @@ module.exports.renderEditForm=async(req,res)=>{
       req.flash("failure","Listing You requested Not Exist");
       return res.redirect("/listings");
     }
-    res.render("listings/edit",{listing});
+    let originalImageUrl=listing.image.url;
+    originalImageUrl=originalImageUrl.replace("/upload","/upload/w_250");
+    res.render("listings/edit",{listing,originalImageUrl});
 }
 
 module.exports.updateListing=async(req,res)=>{
